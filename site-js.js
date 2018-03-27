b2ee9f754f0d6686cce07e578e34b38700fdb928
rajivramarajan <rajivramarajan@gmail.com>
2018-03-27 14:25:43 -0400

    mark

--- site-js.js
+++ site-js.js
@@ -36,21 +36,3 @@ document.getElementById("sidenav").style.left = "0";
 function closeNav() {
 document.getElementById("sidenav").style.left = "-20rem";
 }
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length;
-     i++) {
-    acc[i].onclick = function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling; 
-        if
-            (panel.style.display === "inline"){
-                panel.style.display = "none";}
-        else{
-            panel.style.display ="inline";
-        }
-            }
-    }
-
